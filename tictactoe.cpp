a1b31864033b3685bde57573f569a6981c91cf54
SanjanaVenkat <39311548+SanjanaVenkat@users.noreply.github.com>
2018-10-03 08:45:53 -0700

    Add files via upload
    trying to check for x winning

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,7 +7,7 @@ int main()
   char board [3][3];
   bool playero = true;
   bool playerx = false;
-  bool playing = true;
+  int playing = 0;
   int moves = 0;
   int response = 0;
   bool xwon = false;
@@ -21,15 +21,12 @@ int main()
     }
     cout << endl;
   }
-  while (playing = true) {
-    //if (playing = false) {
-      //cout << "not playing, false" << endl;
-      //}
-    if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
-      xwon == true;
-      playing == false;
-      cout << "test" << endl;
-    }
+
+  while (moves < 9) {
+    // if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+      // xwon == true;
+      // playing == 1;
+      // }
     if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
       xwon == true;
     }
@@ -51,10 +48,8 @@ int main()
     if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')  {
       xwon == true;
     }
- 
-    //}
-  //while (xwon == false && owon == false && moves <= 9) { 
-    if (playing == true && moves == 0 || moves == 2 || moves == 4 || moves == 6 || moves == 8) {
+  
+    while (playing == 0 && moves == 0 || moves == 2 || moves == 4 || moves == 6 || moves == 8) {
     cout << "Player X, enter a row number, 1-3" << endl;
     cin >> row;
     cout << "Enter a column number, 1-3" << endl;
@@ -76,10 +71,29 @@ int main()
       }
       cout << endl;
     }
-      moves++;
+    if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+      playing != 0;
+      playing == 1;
+      cout << "Game won by x"<< endl;
+      cout << "want to play again?" << endl;
+      cin >> response;
+      if (response == 'y') {
+	playing == 0;
+	moves == 0;
+	for (row = 0; row < 3; row++) {
+	  for (col = 0; col < 3; col++) {
+	    cout << board[3][3] << '*';
+	  }
+	  cout << endl;
+	}
+      }
+      if (response == 'n') {
+	cout << "no" << endl;
+      }
+    }
+    moves++;
   }
-  
-   if (playing == true && moves == 1 || moves == 3 || moves == 5 || moves == 7) {
+    if (playing == 0 && moves == 1 || moves == 3 || moves == 5 || moves == 7) {
     cout << "Player O, enter a row number, 1-3" << endl;
     cin >> row;
     cout << "Enter a column number, 1-3" << endl;
@@ -103,32 +117,35 @@ int main()
     }
     moves++;
   }
-  
-  if (playing = true && moves == 9) {
-    playing = false;
+  }
+
+  while (playing == 1) {
+     cout << "not playing" << endl;
+     return 0;
+   }
+   
+  while (playing == 0 && moves == 9) {
+    //playing == false;
     cout << "tie" << endl;
     return 0;
     }
 
-   
-
-
-  if (xwon == true) {
-    cout << "x won" << endl;
-  }
-  if (playing == false) {
+    if (playing == 1) {
     cout << "Not playing" << endl;
     cout << "Play again?" << endl;
     cin >> response;
     if (response == 'n') {
+      cout << "no" << endl;
       return 0;
     }
     else if (response == 'y') {
-      playing == true;
-      moves == 0;
+      cout << "yes" << endl;
+      return 0;
     }
   }
-  }
+ 
 
   return 0;
 }
+
+
